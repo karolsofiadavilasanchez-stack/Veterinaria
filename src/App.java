254fa22093cce6acf79943c9bc17b7fba6a4c7d7
Karol Davila <karolsofiadavilasanchez@gmail.com>
2026-08-28 19:01:41 -0500

    feature: super clase emplados de veterinaria

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        ArrayList<Empleado> empleados = new ArrayList<>();
+        empleados.add(new Veterinario("Juan", 5000));
+ 
     }
 }
---
+++ src/Empleados.java
@@ -0,0 +1,38 @@
+public class Empleados {
+    private String nombre = "";
+    private int edad =0;
+    private double salario =0;
+    private String puesto ="";
+
+    public Empleados(String nombre, int edad, double salario, String puesto) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.salario = salario;
+        this.puesto = puesto;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public int getEdad(){
+        return this.edad;
+    }
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+    public double getSalario(){
+        return this.salario;
+    }
+    public void setSalario(double salario){
+        this.salario = salario;
+    }
+    public String getPuesto(){
+        return this.puesto;
+    }
+    public void setPuesto(String puesto){
+        this.puesto = puesto;
+    }
+    
+}
